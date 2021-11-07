e25db459f1d09d5d018ae2cd16296d70b1a9ca38
Arafat Abdussalam <92020171+ArafatAbdussalam@users.noreply.github.com>
2021-11-07 07:35:38 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,65 @@
+print("Hello")
+print('I am Arafat Abdussalam "A Software Engineeer" based in Nigeria')
+#this is a comment
+
+print("I will now count my chickens:")
+# you need a comma to separate strings from integers
+print("Hens:", 25 + 6 *4)
+#10.0 is an example of floating point number
+# the modulo of a number is its remainder
+print(10%3)
+# a variable is the name for something
+# a variable is stores using the underscore character
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers= 90
+cars_not_driven= cars - drivers
+cars_driven= drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+
+# \ escape character
+
+print("This is a \"double quote\" enclosed in a double quote")
+
+
+print("print three new \t \t \t tab")
+
+print("I am \\ a \\ cat ")
+
+print("bring the \t* fishes here ")
+
+print("this is a \r carriage return")
+
+print("Is this a \f form feed")
+
+print("I know how the ASCII \b backspace works")
+
+#\N unicode• \uxxxx 16 bit-hex value
+#\uxxxxxxxx 32 bit hex value
+#\xhh hex value • \000 octa value
+print("I love the \v vertical tab")
+
+print("how old are you?", end = ' ' )
+age= input ()
+print("how tall are you?", end=' ')
+height= input()
+#pass the argument by using the format function
+print(f"this is your {age} and your {height}")
+
+x= int(input())
+print(f"This is x: {x}")
+
+y= input("Name?  ")
+
+#python 3.6
+#quit()
+#python3.6
+#mkdir lpthw
+#cd lpthw
+#dir
+
